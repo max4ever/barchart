50107bab1b7125c19d5aaec12a4d9489db61a462
max4ever <maxim.postoronca@gmail.com>
2021-04-14 22:25:33 +0200

    added tradingView chart

---
+++ js/tradingviewIframe.js
@@ -0,0 +1,17 @@
+function openTradingViewIframe(stocksList){
+    let iframe = document.getElementById("tradingViewIframe");
+    iframe.src = './html/tradingview.html?stocksList=' + stocksList;
+}
+
+
+function openTradingView(tabulatorTable){
+    let stocksList = tabulatorTable.getGroups().flatMap(row => row.getKey()).filter(s => s != "" && s.length < 10);
+    document.getElementById('tradingView-wrapper').style.display = 'block';
+    openTradingViewIframe(stocksList);
+
+    return false;
+}
+
+function closeTradingViewIframe(){
+    document.getElementById('tradingView-wrapper').style.display = 'none';
+}
